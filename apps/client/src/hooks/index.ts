6568d23059a329265262ee52b9206d795eb4b3c1
Mayank Sharma <sharmamayank01010@gmail.com>
2026-08-29 15:35:00 +0530

    refactor(hooks): export shared utility hooks from barrel index

---
+++ apps/client/src/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from "./useQueryParams";
+export * from "./useDebounce";
+export * from "./useMediaQuery";
+export * from "./useLocalStorage";
+export * from "./useIntersectionObserver";
+export * from "./useClickOutside";
+export * from "./useImage";
